09fd35e59a0030230f2f2098309cf943ba41950b
yuze.xia <327932801@qq.com>
2018-08-18 10:43:38 +0800

    add theme switch

---
+++ theme-switch/src/Content.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Content extends Component {
+    render() {
+        return (
+            <div>React.js小书</div>
+        )
+    }
+}
+
+export default content
